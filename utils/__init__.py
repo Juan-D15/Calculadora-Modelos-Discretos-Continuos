260188877d2ca411947e1c38e36b55f4f70b0571
Juan Díaz <jchendiaz15@gmail.com>
2026-03-18 11:25:10 -0600

    chore: export Poisson approximation classes

--- utils/__init__.py
+++ utils/__init__.py
@@ -21,6 +21,12 @@ from utils.calculos import (
     calcular_probabilidad_acumulada_hipergeometrica,
     calcular_probabilidades_acumuladas_hipergeometrica,
     buscar_valor_tolerancia,
+    calcular_sesgo_poisson,
+    calcular_curtosis_poisson,
+    calcular_desviacion_poisson,
+    calcular_media_poisson,
+    calcular_probabilidades_poisson,
+    poisson_pmf,
 )
 
 from utils.validaciones import (
@@ -29,9 +35,17 @@ from utils.validaciones import (
     parsear_valores_x,
     validar_parametros_comparacion,
     validar_tolerancia,
+    validar_condiciones_poisson,
+    parsear_valores_x_poisson,
+    validar_valores_x_poisson,
 )
 
 from utils.formato import generar_texto_resultados, calcular_moda, generar_resumen_corto
+
+from utils.aproximacion_poisson import (
+    AproximacionPoissonBinomial,
+    AproximacionPoissonHiper,
+)
 
 from utils.file_loader import (
     FileLoader,
@@ -69,10 +83,22 @@ __all__ = [
     "parsear_valores_x",
     "validar_parametros_comparacion",
     "validar_tolerancia",
+    "calcular_sesgo_poisson",
+    "calcular_curtosis_poisson",
+    "calcular_desviacion_poisson",
+    "calcular_media_poisson",
+    "calcular_probabilidades_poisson",
+    "poisson_pmf",
+    "validar_condiciones_poisson",
+    "parsear_valores_x_poisson",
+    "validar_valores_x_poisson",
     # Formato
     "generar_texto_resultados",
     "calcular_moda",
     "generar_resumen_corto",
+    # Aproximaciones
+    "AproximacionPoissonBinomial",
+    "AproximacionPoissonHiper",
     # FileLoader
     "FileLoader",
     "ErrorCargaArchivo",
